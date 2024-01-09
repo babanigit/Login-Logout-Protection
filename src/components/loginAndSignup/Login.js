2c8494bb1820bea24bf7333616b3a9c70256e86a
bablu <aniketpanchal07@gmail.com>
2024-01-09 12:32:35 +0530

    modified

--- src/components/loginAndSignup/Login.js
+++ src/components/loginAndSignup/Login.js
@@ -86,12 +86,12 @@ function Login() {
         </div> */}
 
     <div className='w-full h-screen'>
-      <img 
-        className='hidden sm:block absolute w-full h-full object-cover bg-black'
+      {/* <img 
+        className='hidden sm:block absolute w-full h-full object-cover bg-white'
         // src={wallpaper2}
         alt='/'
-      />
-      <div className='bg-black/60 fixed top-0 left-0 w-full h-screen text-red-50 flex justify-center'></div>
+      /> */}
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-screen text-red-50 flex justify-center'> </div>
       <div className='fixed w-full px-4 py-24 z-50'>
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <div className='max-w-[320px] mx-auto py-16'>
