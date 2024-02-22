b36c8732805cad3d72942e3d0e6fe702ee4a1859
bablu <aniketpanchal07@gmail.com>
2024-02-23 00:19:36 +0530

    added new file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey1: "AIzaSyB1VExSlrVY1Ah6fgIXoALGfkN-Jkyqjq0",
+    authDomain1: "neon-fort-374613.firebaseapp.com",
+    projectId1: "neon-fort-374613",
+    storageBucket1: "neon-fort-374613.appspot.com",
+    messagingSenderId1: "767257243624",
+    appId1: "1:767257243624:web:a637ef731ed8d5cc977e5d"
+  }
